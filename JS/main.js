358e30c4c1b06458ab5aea99c94adaf6665ef0f9
recarlin <recarlin@live.com>
2016-09-14 14:23:25 -0400

    Version 1
    Added the core functions. Can read and write XML.

--- JS/main.js
+++ JS/main.js
@@ -1,59 +1,185 @@
+function addGroup() {
+	var container = document.getElementById('whole_container');
+	var table_container = document.createElement('div');
+	var title_container = document.createElement('div');
+	var table = document.createElement('table');
+	
+	table_container.className = 'table_container';
+	title_container.className = 'title_container';
+	table.className = 'group';
+	
+	title_container.innerHTML = '<input class="group_name" type="text" placeholder="Group Name"><a href="#" class="delete_button" onclick="deleteGroup(this)">Delete Group</a></div>';
+	table.innerHTML = '<tr class="contact"><td class="button_container"><a href="#" class="add_button"  onclick="addContact(this)">Add Contact</a></td></tr>';
+	
+	table_container.appendChild(title_container);
+	table_container.appendChild(table);
+	container.appendChild(table_container);
+}
+
+function deleteGroup(button) {
+	var titleNode = button.parentNode;
+	var container = titleNode.parentNode;
+	var whole = container.parentNode;
+
+	whole.removeChild(container);
+}
+
+function addContact(newRow) {
+	var table = newRow.closest('table');
+	var row = newRow.closest('tr');
+	var rowIndex = row.rowIndex;
+	var blankIndex = rowIndex + 1;
+	var newRow = table.insertRow(rowIndex);
+	var cell1 = newRow.insertCell(0);
+	var cell2 = newRow.insertCell(1);
+	var cell3 = newRow.insertCell(2);
+	var cell4 = newRow.insertCell(3);
+	var cell5 = newRow.insertCell(4);
+	
+	cell1.innerHTML = '<td><input class="contact_name" type="text" placeholder="Contact Name"></td>';
+	cell2.innerHTML = '<td><input class="number" type="text" placeholder="Phone Number"></td>';
+	cell3.innerHTML = '<td><input class="number" type="text" placeholder="Phone Number"></td>';
+	cell4.innerHTML = '<td><input class="number" type="text" placeholder="Phone Number"></td>';
+	cell5.innerHTML = '<td class="button_container"><a href="#" class="delete_button" onclick="deleteContact(this)">Delete Contact</a></td>';
+	
+	var newBlank = table.insertRow(blankIndex);
+	var cell1_blank = newBlank.insertCell(0);
+	
+	cell1_blank.innerHTML = '<tr class="blank_row"><td colspan="3"></td></tr>';
+}
+
 function deleteContact(contact) {
 	var table = contact.closest('table');
-	var row = contact.parentNode.parentNode;
+	var row = contact.closest('tr');
 	var rowIndex = row.rowIndex;
-	var blankIndex = row + 1;
+	var blankIndex = rowIndex + 1;
+	table.deleteRow(blankIndex);
 	table.deleteRow(rowIndex);
-	table.deleteRow()
 }
-// Original JavaScript code by Chirp Internet: www.chirp.com.au
-// Please acknowledge use of this code by including this header.
 
-document.getElementById("modal_feedback").addEventListener("submit", function(e) {
-	var form = this;
-	if(form.name.value == "") {
-		alert("Please enter your Name");
-		form.name.focus();
-		e.preventDefault();
-	} else if(form.email.value == "") {
-		alert("Please enter a valid Email address");
-		form.email.focus();
-		e.preventDefault();
-	} else if(form.message.value == "") {
-		alert("Please enter your comment or question in the Message box");
-		form.message.focus();
-		e.preventDefault();
-	}
-	}, false);
+function readXML() {
+	$(xml).find('Menu').each(function(){
+		$name = $(this).attr('Name');
+		
+		var container = document.getElementById('whole_container');
+		var table_container = document.createElement('div');
+		var title_container = document.createElement('div');
+		var table = document.createElement('table');
+		
+		table_container.className = 'table_container';
+		title_container.className = 'title_container';
+		table.className = 'group';
+		
+		title_container.innerHTML = '<input class="group_name" type="text" placeholder="' + $name + '"><a href="#" class="delete_button" onclick="deleteGroup(this)">Delete Group</a></div>';
+		table.innerHTML = '<tr class="contact"><td class="button_container"><a href="#" class="add_button"  onclick="addContact(this)">Add Contact</a></td></tr>';
 	
-	document.addEventListener("DOMContentLoaded", function() {
-	var modalWrapper = document.getElementById("modal_wrapper");
-	var modalWindow  = document.getElementById("modal_window");
+		table_container.appendChild(title_container);
+		table_container.appendChild(table);
+		container.appendChild(table_container);
+		
+		$(this).find('Unit').each(function(){
+			$contact = $(this).attr('Name');
+			$p1 = $(this).attr('Phone1');
+			$p2 = $(this).attr('Phone2');
+			$p3 = $(this).attr('Phone3');
+		});
+	});	
 	
-	var openModal = function(e) {
-		modalWrapper.className = "overlay";
-		modalWindow.style.marginTop = (-modalWindow.offsetHeight)/2 + "px";
-		modalWindow.style.marginLeft = (-modalWindow.offsetWidth)/2 + "px";
-		e.preventDefault();
-	};
+/*
+	var table = newRow.closest('table');
+	var row = newRow.closest('tr');
+	var rowIndex = row.rowIndex;
+	var blankIndex = rowIndex + 1;
+	var newRow = table.insertRow(rowIndex);
+	var cell1 = newRow.insertCell(0);
+	var cell2 = newRow.insertCell(1);
+	var cell3 = newRow.insertCell(2);
+	var cell4 = newRow.insertCell(3);
+	var cell5 = newRow.insertCell(4);
 	
-	var closeModal = function(e) {
-		modalWrapper.className = "";
-		e.preventDefault();
-	};
+	cell1.innerHTML = '<td><input class="contact_name" type="text" placeholder="Contact Name"></td>';
+	cell2.innerHTML = '<td><input class="number" type="text" placeholder="Phone Number"></td>';
+	cell3.innerHTML = '<td><input class="number" type="text" placeholder="Phone Number"></td>';
+	cell4.innerHTML = '<td><input class="number" type="text" placeholder="Phone Number"></td>';
+	cell5.innerHTML = '<td class="button_container"><a href="#" class="delete_button" onclick="deleteContact(this)">Delete Contact</a></td>';
 	
-	var clickHandler = function(e) {
-		if(e.target.tagName == "DIV") {
-			if(e.target.id != "modal_window") closeModal(e);
-		}
-	};
+	var newBlank = table.insertRow(blankIndex);
+	var cell1_blank = newBlank.insertCell(0);
 	
-	var keyHandler = function(e) {
-		if(e.keyCode == 27) closeModal(e);
-	};
+	cell1_blank.innerHTML = '<tr class="blank_row"><td colspan="3"></td></tr>';
+*/
+}
+
+var xml = "phonebook.xml"; //How to set path this line?
+
+
+$.ajax({
+	type: "GET",
+	url: "phonebook.xml",
+	data: jsonData,
+	success: function(xml){
 	
-	document.getElementById("modal_open").addEventListener("click", openModal, false);
-	document.getElementById("modal_close").addEventListener("click", closeModal, false);
-	document.addEventListener("click", clickHandler, false);
-	document.addEventListener("keydown", keyHandler, false);
-}, false);
+	$(xml).find('Menu').each(function(){
+		$name = $(this).attr('Name');
+		
+		var container = document.getElementById('whole_container');
+		var table_container = document.createElement('div');
+		var title_container = document.createElement('div');
+		var table = document.createElement('table');
+		
+		table_container.className = 'table_container';
+		title_container.className = 'title_container';
+		table.className = 'group';
+		
+		title_container.innerHTML = '<input class="group_name" type="text" placeholder="' + $name + '"><a href="#" class="delete_button" onclick="deleteGroup(this)">Delete Group</a></div>';
+		table.innerHTML = '<tr class="contact"><td class="button_container"><a href="#" class="add_button"  onclick="addContact(this)">Add Contact</a></td></tr>';
+	
+		table_container.appendChild(title_container);
+		table_container.appendChild(table);
+		container.appendChild(table_container);
+		
+		$(xml).find('Unit').each(function(){
+			$contact = $(this).attr('Name');
+			$p1 = $(this).attr('Phone1');
+			$p2 = $(this).attr('Phone2');
+			$p3 = $(this).attr('Phone3');
+		});
+	},
+    error: function(jqXhr, textStatus, errorThrown){
+        alert(errorThrown);
+    }
+});
+
+
+
+/*
+"<note>" +
+"<to>Tove</to>" +
+"<from>Jani</from>" +
+"<heading>Reminder</heading>" +
+"<body>Don't forget me this weekend!</body>" +
+"</note>";
+
+
+<?xml version="1.0" encoding="UTF-8"?>
+<YealinkIPPhoneBook>
+<Title>Phonebook</Title>
+	<Menu Name="Bradner">
+		<Unit Name="Adam Brossia" Phone3="" Phone2="4192069454" Phone1="1702"/>
+		<Unit Name="Bradner Office" Phone3="" Phone2="4192873275" Phone1="1701"/>
+		<Unit Name="Brock Abke" Phone3="" Phone2="4193456578" Phone1=""/>
+		<Unit Name="Cameron Shirer" Phone3="" Phone2="4193418510" Phone1=""/>
+		<Unit Name="Jim Walter" Phone3="" Phone2="4199341693" Phone1="1703"/>
+	</Menu>
+	<Menu Name="Curtice">
+		<Unit Name="Brice Gahler" Phone3="" Phone2="4195520169" Phone1="3103"/>
+		<Unit Name="Chris Milbrodt" Phone3="" Phone2="4196997538" Phone1=""/>
+	</Menu>
+	<Menu Name="Lemonye">
+		<Unit Name="Russell Carlin" Phone3="" Phone2="4192069454" Phone1="1054"/>
+		<Unit Name="Lemoyne Office" Phone3="" Phone2="4192873275" Phone1="1031"/>
+		<Unit Name="Tim Alexander" Phone3="" Phone2="4193456578" Phone1="1030"/>
+		<Unit Name="Mark Radabaugh" Phone3="" Phone2="4193418510" Phone1="1021"/>
+	</Menu>
+</YealinkIPPhoneBook>
+*/
